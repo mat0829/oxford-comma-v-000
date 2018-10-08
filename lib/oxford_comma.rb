6423d3306c850ee23e8a3f345540d3163be810ae
David Anderson <matandersonlv@hotmail.com>
2018-10-08 18:28:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array. length == 2 
     array.join(" and ")
   elsif array.length == 3
-    array.join(", ")<<("and ")
+    array.join(", ")
   elsif array.length == 1
     array.join
     
