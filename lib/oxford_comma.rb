b77b010861540afb6a76183f453e1b1d8f5171f7
David Anderson <matandersonlv@hotmail.com>
2018-10-08 18:36:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array. length == 2 
     array.join(" and ")
   elsif array.length > 2 
-    array.join(", ").pop("and")
+    new array = array
   elsif array.length == 1
     array.join
     
