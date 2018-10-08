8bcd4498082cd44e3095fb0a4c05435d25913eb5
David Anderson <matandersonlv@hotmail.com>
2018-10-08 18:41:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    new_array = array
+    final_element = new_array.pop()
+    "#{new_array.join(", ")}, and #{final_element}"
+  end
 end
